951150979daeafff1bb1bea408b9390d813f6afe
Dennis Lai <dshlai@gmail.com>
2021-11-17 22:47:02 +0800

    change training code

--- HW3/train_word2vec_big.py
+++ HW3/train_word2vec_big.py
@@ -2,10 +2,15 @@ import pandas as pd
 from gensim.models import Word2Vec, Phrases
 import spacy
 from multiprocessing import Pool
-import sys
+import os
 
 nlp = spacy.load("en_core_web_md")
 stop_words = nlp.Defaults.stop_words
+
+train_path = "../../models/P76I900/HW3/"
+if not os.path.exists(train_path):
+    print("Create model save path: {}".format(train_path))
+    os.mkdir(train_path)
 
 def train():
     
@@ -17,8 +22,10 @@ def train():
         train_texts = p.map(tokenize_single_doc, texts)
 
     bigram = Phrases(train_texts)
-    model = Word2Vec(bigram[train_texts], sg=1, vector_size=200, min_count=8, workers=12)
-    model.save("skip_model_cord19_covid_55k.bin")
+    model = Word2Vec(bigram[train_texts], sg=1, window=10,
+                     vector_size=300, min_count=5, workers=12)
+
+    model.save("{}skip_lemmatized_model_cord19_covid_55k.bin".format(train_path))
 
 
 def tokenize_single_doc(doc):
@@ -27,7 +34,7 @@ def tokenize_single_doc(doc):
     except TypeError:
         return []
 
-    tks = [tk.text for tk in doc if tk.is_alpha]
+    tks = [tk.lemma_ for tk in doc if tk.is_alpha]
     tks = [tk for tk in tks if not tk in stop_words]
     return tks
 
